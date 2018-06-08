0730a69fc48ff863aec66ddb391c198284db8ddc
baskar <kbhaskaR1228@GMAIL.COM>
2018-06-09 01:17:17 +0530

    new files added

---
+++ src/main/java/com/vrd/sdt/solutiondesignservice/SolutionDesignServiceApplication.java
@@ -0,0 +1,12 @@
+package com.vrd.sdt.solutiondesignservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionDesignServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionDesignServiceApplication.class, args);
+	}
+}
